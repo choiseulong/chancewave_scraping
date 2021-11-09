c3f9b3e9f80fac282d52a797fcc9c38fa96b10a0
choiseulong <choiseulong@gmail.com>
2021-11-09 19:43:45 +0900

    :wrench: seoul_woman_0,1,2 합병후 나머지 파일 삭제

--- scrapingProject/workers/dataScraper/scraper/seoul_woman_up/seoul_woman_up_1.py
+++
@@ -1,7 +0,0 @@
-from .seoul_woman_up_0 import Scraper as default_scraper
-
-class Scraper(default_scraper):
-    def __init__(self, session):
-        super().__init__(session)
-        self.postUrl = 'https://www.seoulwomanup.or.kr/womanup/common/bbs/selectBBS.do?bbs_seq={}&bbs_code=centerall'
-
