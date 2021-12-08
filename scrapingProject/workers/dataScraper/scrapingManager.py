fc48bd4e637932a35195ba6adca6a3fab525d923
choiseulong <choiseulong@gmail.com>
2021-12-08 20:31:20 +0900

    :sparkles: 채널 moef_0 스크래퍼 추가

--- scrapingProject/workers/dataScraper/scrapingManager.py
+++ scrapingProject/workers/dataScraper/scrapingManager.py
@@ -52,7 +52,7 @@ class ScrapingManager:
                 # something = checker.is_handling(traceback.format_exc(), e.__class__)
                 # print(something)
             
-            if channelCode != 'nssc_0':
+            if channelCode != 'moef_0':
                 continue
             print(channelCode, 'init')
             session = self.get_requests_session()
