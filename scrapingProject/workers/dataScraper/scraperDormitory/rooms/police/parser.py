d6cbb3d296ebc0c1c0aada34eda0a23d02d087d8
choiseulong <choiseulong@gmail.com>
2021-12-10 16:56:15 +0900

    :sparkles: 채널 police_0 스크래퍼 추가

---
+++ scrapingProject/workers/dataScraper/scraperDormitory/rooms/police/parser.py
@@ -0,0 +1,79 @@
+from workers.dataScraper.scraperDormitory.parserTools.tools import *
+
+def postListParsingProcess(**params):
+    targetKeyInfo = {
+        'multipleType' : ['postUrl', 'postTitle', 'uploader', 'uploadedTime', 'viewCount']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    tbody = extract_children_tag(soup, 'tbody', dummyAttrs, childIsNotMultiple)
+    trList = extract_children_tag(tbody, 'tr', dummyAttrs, childIsMultiple)
+    for tr in trList:
+        tdList = extract_children_tag(tr, 'td', dummyAttrs, childIsMultiple)
+        for tdIdx, td in enumerate(tdList):
+            tdText = extract_text(td)
+            if tdIdx == 1:
+                aTag = extract_children_tag(td, 'a', dummyAttrs, childIsNotMultiple)
+                postId = parse_onclick(extract_attrs(aTag, 'onclick'))
+                var['postUrl'].append(
+                    var['postUrlFrame'].format(postId)
+                )
+                var['postTitle'].append(tdText)
+            elif tdIdx == 3:
+                var['uploader'].append(tdText)
+            elif tdIdx == 4:
+                var['uploadedTime'].append(
+                    convert_datetime_string_to_isoformat_datetime(tdText)
+                )
+            elif tdIdx == 5:
+                var['viewCount'].append(
+                    extract_numbers_in_text(tdText)
+                )
+
+    valueList = [var[key] for key in keyList]
+    result = merge_var_to_dict(keyList, valueList)
+    # print(result)
+    return result
+
+def parse_onclick(text):
+    return re.findall("'(.+?)'", text)[0]
+
+def postContentParsingProcess(**params):
+    targetKeyInfo = {
+        'singleType' : ['contact', 'postText', 'startDate', 'endDate', 'startDate2', 'endDate2'],
+        'multipleType' : ['postImageUrl']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    tbody = extract_children_tag(soup, 'tbody', dummyAttrs, childIsNotMultiple)
+    thList = extract_children_tag(tbody, 'th', dummyAttrs, childIsMultiple)
+    dateCount = 0
+    dateInfo = {'startDate' : '공지시작일', 'endDate' : '공지종료일', 'startDate2' : '게시시작일시', 'endDate2' : '게시종료일시'}
+    for th in thList:
+        thText = extract_text(th)
+        for key in dateInfo:
+            if dateInfo[key] in thText:
+                dateString = extract_text(find_next_tag(th))
+                if dateString:
+                    var[key] = convert_datetime_string_to_isoformat_datetime(dateString)
+                dateCount += 1
+        if dateCount == 4:
+            break
+    board_contents = extract_children_tag(tbody, 'div', {'class' : 'board-contents'}, childIsNotMultiple)
+    postText = extract_text(board_contents)
+    var['postText'] = clean_text(postText)
+    var['contact'] = extract_contact_numbers_from_text(postText)
+    imgList = extract_children_tag(board_contents, 'img', {'src' : True}, childIsMultiple)
+    if imgList:
+        for img in imgList:
+            src = extract_attrs(img, 'src')
+            if 'base64' in src :
+                var['postImageUrl'].append(src)
+                continue
+            if 'http' not in src :
+                src = var['channelMainUrl'] + src
+                var['postImageUrl'].append(src)
+                continue
+
+    valueList = [var[key] for key in keyList]
+    result = convert_merged_list_to_dict(keyList, valueList)
+    print(result)
+    return result
