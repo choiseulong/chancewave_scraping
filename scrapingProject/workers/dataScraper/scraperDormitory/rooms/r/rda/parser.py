7b7ff2d2a6eb29a18431c61c34e305c04831c818
choiseulong <choiseulong@gmail.com>
2021-12-14 18:22:43 +0900

    :sparkles: 채널 rda_0 스크래퍼 추가

---
+++ scrapingProject/workers/dataScraper/scraperDormitory/rooms/r/rda/parser.py
@@ -0,0 +1,84 @@
+from workers.dataScraper.scraperDormitory.parserTools.tools import *
+
+def postListParsingProcess(**params):
+    targetKeyInfo = {
+        'multipleType' : ['postUrl', 'postTitle', 'uploader', 'uploadedTime', 'viewCount']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    tbody = extract_children_tag(soup, 'tbody', dummyAttrs, childIsNotMultiple)
+    trList = extract_children_tag(tbody, 'tr', {'class' : False}, childIsMultiple)
+    for tr in trList :
+        title = extract_children_tag(tr, 'a', {'title' : True}, childIsNotMultiple)
+        var['postTitle'].append(extract_text(title))
+        var['postUrl'].append(
+            var['postUrlFrame'].format(extract_text_between_prefix_and_suffix(
+                    'dataNo=', '&mode',
+                    extract_attrs(
+                        title,
+                        'href'
+                    )
+                )
+            )
+        )
+        var['uploader'].append(
+            extract_text(
+                extract_children_tag(tr, 'td', {'class' : 'board_tb_part'}, childIsNotMultiple)
+            )
+        )
+        var['uploadedTime'].append(
+            convert_datetime_string_to_isoformat_datetime(
+                extract_text(
+                    extract_children_tag(tr, 'td', {'class' : 'board_tb_date'}, childIsNotMultiple)
+                )
+            )
+        )
+        var['viewCount'].append(
+            extract_numbers_in_text(
+                extract_text(
+                    extract_children_tag(tr, 'td', {'class' : 'board_tb_hit'}, childIsNotMultiple)
+                )
+            )
+        )
+    valueList = [var[key] for key in keyList] 
+    result = merge_var_to_dict(keyList, valueList)
+    # print(result)
+    return result
+
+
+def postContentParsingProcess(**params):
+    targetKeyInfo = {
+        'singleType' : ['contact', 'postText'],
+        'multipleType' : ['postImageUrl']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    tbody = extract_children_tag(soup, 'tbody', dummyAttrs, childIsNotMultiple)
+    thList = extract_children_tag(tbody, 'th', dummyAttrs, childIsMultiple)
+    for th in thList:
+        thText = extract_text(th)
+        if '전화번호' in thText:
+            var['contact'] = extract_contact_numbers_from_text(
+                extract_text(
+                    find_next_tag(th)
+                )
+            )
+            break
+    
+    contents = extract_children_tag(tbody, 'td', {'class' : 'con'}, childIsNotMultiple)
+    var['postText'] = clean_text(extract_text(contents))
+    imgList = extract_children_tag(contents, 'img', {'src' : True}, childIsMultiple)
+    if imgList:
+        for img in imgList:
+            src = extract_attrs(img, 'src')
+            if 'base64' in src :
+                var['postImageUrl'].append(src)
+                continue
+            if 'http' not in src :
+                src = var['channelMainUrl'] + src
+                var['postImageUrl'].append(src)
+                continue
+
+    valueList = [var[key] for key in keyList] 
+    result = convert_merged_list_to_dict(keyList, valueList)
+    
+    # print(result)
+    return result
---
+++ scrapingProject/workers/dataScraper/scraperDormitory/rooms/r/rda/scraper.py
@@ -0,0 +1,60 @@
+from workers.dataScraper.scraperDormitory.scraping_default_usage import Scraper as ABCScraper
+from workers.dataScraper.scraperDormitory.scraperTools.tools import *
+from .parser import *
+
+# 채널 이름 : 농림축산식품부
+
+# 타겟 : 모든 공고
+# 중단 시점 : 마지막 페이지 도달시
+
+#HTTP Request
+'''
+    @post list
+
+    method : GET
+    url =  https://www.rda.go.kr/board/board.do?prgId=nei_ancmttEntry&currPage=1
+    header :
+        None
+
+'''
+'''
+    @post info
+    method : GET
+    url : 'https://www.rda.go.kr/board/board.do?boardId=ancmtt&prgId=nei_ancmttEntry&dataNo={postId}'
+    header :
+        None
+
+'''
+sleepSec = 2
+isUpdate = True
+
+class Scraper(ABCScraper):
+    def __init__(self, session):
+        super().__init__(session)
+        self.channelMainUrl = 'https://www.rda.go.kr'
+        self.postUrl = 'https://www.rda.go.kr/board/board.do?boardId=ancmtt&prgId=nei_ancmttEntry&menu_id=pun&currPage=1&dataNo={}&mode=updateCnt'
+        
+    def scraping_process(self, channelCode, channelUrl, dateRange):
+        super().scraping_process(channelCode, channelUrl, dateRange)
+        self.session = set_headers(self.session)
+        self.pageCount = 1
+        while True :
+            self.channelUrl = self.channelUrlFrame.format(self.pageCount)
+            self.post_list_scraping()
+            if self.scrapingTarget :
+                self.target_contents_scraping()
+                self.collect_data()
+                self.mongo.reflect_scraped_data(self.collectedDataList)
+                self.pageCount += 1
+            else :
+                break
+
+    def post_list_scraping(self):
+        data = {
+            "page" : self.pageCount,
+            "row" : 50
+        }
+        super().post_list_scraping(postListParsingProcess, 'post', data, sleepSec)
+
+    def target_contents_scraping(self):
+        super().target_contents_scraping(postContentParsingProcess, sleepSec)
