d788be557e88b4f99e7d9072da086eef88a4751f
choiseulong <choiseulong@gmail.com>
2021-12-20 18:33:43 +0900

    :test: 김해시 스크래퍼 구성중

--- scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongnam/gimhae/parser.py
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongnam/gimhae/parser.py
@@ -2,38 +2,49 @@ from workers.data_scraper.scraper_dormitory.parser_tools.tools import *
 
 def postListParsingProcess(**params):
     targetKeyInfo = {
-        'multipleType' : ['postUrl', 'postTitle', 'uploadedTime', 'viewCount', 'uploader']
+        'multipleType' : ['postUrl', 'postTitle', 'uploadedTime', 'viewCount', 'uploader', 'postSubject']
     }
     var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
-    tbody = extract_children_tag(soup, 'tbody', dummyAttrs, childIsNotMultiple)
-    trList = extract_children_tag(tbody, 'tr', dummyAttrs, childIsMultiple)
-    for tr in trList:
-        tdList = extract_children_tag(tr, 'td', dummyAttrs, childIsMultiple)
-        for tdIdx, td in enumerate(tdList):
-            tdText = extract_text(td)
-            if tdIdx == 1 : 
-                var['postTitle'].append(tdText)
-                aTag = extract_children_tag(td, 'a', dummyAttrs, childIsNotMultiple)
-                href = extract_attrs(aTag, 'href')
-                var['postUrl'].append(
-                    var['channelMainUrl'] + href
-                )
-            elif tdIdx == 3 :
-                var['uploader'].append(tdText)
-            elif tdIdx == 2 :
+    liList = extract_children_tag(soup, 'li', {'class' : 'li1'}, childIsMultiple)
+    for li in liList:
+        em = extract_children_tag(li, 'em', dummyAttrs, childIsNotMultiple)
+        if em:
+            postSubject = extract_text(em)[1:-1]
+        else :
+            postSubject = ''
+        var['postSubject'].append(postSubject)
+        strong = extract_children_tag(li, 'strong', dummyAttrs, childIsNotMultiple)
+        postTitle = extract_text(strong).replace('새 글', '').replace(postSubject, '').strip()
+        var['postTitle'].append(postTitle)
+        spanList = extract_children_tag(li, 'span', {'class' : 't3'}, childIsMultiple)
+        uploader = ''
+        for spanIdx, span in enumerate(spanList):
+            spanText = extract_text(span)
+            if spanIdx == 0:
                 var['uploadedTime'].append(
-                    convert_datetime_string_to_isoformat_datetime(tdText)
+                    convert_datetime_string_to_isoformat_datetime(
+                        spanText
+                    )
                 )
-            elif tdIdx == 5 :
+            elif spanIdx in [1, 2] :
+                uploader += spanText + ' '
+            elif spanIdx == 3 :
                 var['viewCount'].append(
-                    extract_numbers_in_text(tdText)
+                    extract_numbers_in_text(spanText)
                 )
+        var['uploader'].append(uploader)
+        aTag = extract_children_tag(li, 'a', dummyAttrs, childIsNotMultiple)
+        href = extract_attrs(aTag, 'href')
+        var['postUrl'].append(
+            var['postUrlFrame'] + href
+        )
+            
 
     valueList = [var[key] for key in keyList]
     result = merge_var_to_dict(keyList, valueList)
-    # print(result)
-    return result
-
+    print(result)
+    # return result
+#### contents 처리해야함
 def postContentParsingProcess(**params):
     targetKeyInfo = {
         'singleType' : ['contact', 'postText'],
