b1feea7c10ef75044c49a6ea59170197ac8b37da
choiseulong <choiseulong@gmail.com>
2021-12-20 15:17:06 +0900

    :sparkles: 채널 mss_0 스크래퍼 추가

---
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/central_government/mss/parser.py
@@ -0,0 +1,65 @@
+from typing_extensions import Literal
+from workers.data_scraper.scraper_dormitory.parser_tools.tools import *
+import re
+
+def postListParsingProcess(**params):
+    targetKeyInfo = {
+        'multipleType' : ['postUrl', 'postTitle', 'uploadedTime', 'viewCount', 'uploader']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    tbody = extract_children_tag(soup, 'tbody', dummyAttrs, childIsNotMultiple)
+    trList = extract_children_tag(tbody, 'tr', dummyAttrs, childIsMultiple)
+    for tr in trList:
+        tdList = extract_children_tag(tr, 'td', dummyAttrs, childIsMultiple)
+        for tdIdx, td in enumerate(tdList):
+            tdText = extract_text(td)
+            if tdIdx == 1 : 
+                var['postTitle'].append(tdText)
+                aTag = extract_children_tag(td, 'a', dummyAttrs, childIsNotMultiple)
+                onclick = extract_attrs(aTag, 'onclick')
+                postId = parse_onclick(onclick)
+                var['postUrl'].append(
+                    var['postUrlFrame'].format(postId)
+                )
+            elif tdIdx == 2 :
+                var['uploader'].append(tdText)
+            elif tdIdx == 4 :
+                var['uploadedTime'].append(tdText)
+            elif tdIdx == 5 :
+                var['viewCount'].append(
+                    extract_numbers_in_text(tdText)
+                )
+
+    valueList = [var[key] for key in keyList]
+    result = merge_var_to_dict(keyList, valueList)
+    return result
+
+def parse_onclick(text):
+    return re.findall("'(.+?)'", text)[1]
+
+
+def postContentParsingProcess(**params):
+    targetKeyInfo = {
+        'singleType' : ['contact', 'postText'],
+        'multipleType' : ['postImageUrl']
+    }
+    var, _, keyList, fullText = html_type_default_setting(params, targetKeyInfo)
+    fullText = fullText.replace('&lt;', '<').replace('&gt;', '>').replace('&middot;', '·')
+    soup = change_to_soup(fullText)
+
+    contents_box = extract_children_tag(soup, 'div', {'class' : 'view_contents'}, childIsNotMultiple)
+    postText = extract_text(contents_box)
+    var['contact'] = extract_contact_numbers_from_text(postText)
+    var['postText'] = erase_html_tags(clean_text(postText))
+    imgList = extract_children_tag(contents_box, 'img', {'src' : True}, childIsMultiple)
+    if imgList:
+        for img in imgList:
+            src = extract_attrs(img, 'src')
+            if 'http' not in src and 'base64' not in src :
+                src = var['channelMainUrl'] + src
+            var['postImageUrl'].append(src)
+ 
+    valueList = [var[key] for key in keyList]
+    result = convert_merged_list_to_dict(keyList, valueList)
+    # print(result)
+    return result
--- scrapingProject/workers/data_scraper/scraping_manager.py
+++ scrapingProject/workers/data_scraper/scraping_manager.py
@@ -56,7 +56,7 @@ class scraping_manager:
                 # something = checker.is_handling(traceback.format_exc(), e.__class__)
                 # print(something)
             
-            if channelCode != 'kcg_0':
+            if channelCode != 'mss_0':
                 continue
             print(channelCode, 'init')
             session = self.get_requests_session()
