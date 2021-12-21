9f884ca7feaba1c5713654c231612e11b688693c
choiseulong <choiseulong@gmail.com>
2021-12-21 17:57:10 +0900

    :sparkles: 채널 namhae_0 스크래퍼 추가

--- scrapingProject/workers/data_scraper/scraping_manager.py
+++ scrapingProject/workers/data_scraper/scraping_manager.py
@@ -56,7 +56,7 @@ class scraping_manager:
                 # something = checker.is_handling(traceback.format_exc(), e.__class__)
                 # print(something)
             
-            if channelCode != 'goseong_0':
+            if channelCode != 'namhae_0':
                 continue
             print(channelCode, 'init')
             print(groupName, roomName)
