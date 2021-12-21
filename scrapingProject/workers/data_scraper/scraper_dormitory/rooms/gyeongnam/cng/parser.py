ed98aa8d5d90d9df93730c1c897f932b2572c34f
choiseulong <choiseulong@gmail.com>
2021-12-21 18:45:27 +0900

    :sparkles: 채널 cng_0 스크래퍼 추가

---
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongnam/cng/parser.py
@@ -0,0 +1,75 @@
+from workers.data_scraper.scraper_dormitory.parser_tools.tools import *
+
+def postListParsingProcess(**params):
+    targetKeyInfo = {
+        'multipleType' : ['postUrl', 'postTitle', 'uploadedTime', 'viewCount', 
+        'uploader', 'postThumbnail', 'postSubject']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    liList = extract_children_tag(soup, 'li', {'class' : 'li1'}, childIsMultiple)
+    for li in liList:
+        aTag = extract_children_tag(li, 'a', dummyAttrs, childIsNotMultiple)
+        href = extract_attrs(aTag, 'href')
+        postId = extract_text_between_prefix_and_suffix('idx=', '&amode', href)
+        var['postUrl'].append(
+            var['postUrlFrame'].format(postId)
+        )
+        strong = extract_children_tag(li, 'strong', {'class' : 't1'}, childIsNotMultiple)
+        var['postTitle'].append(
+            extract_text(strong)
+        )
+        postThumbnail = extract_children_tag(li, 'img', dummyAttrs, childIsNotMultiple)
+        if postThumbnail:
+            src = extract_attrs(postThumbnail, 'src')
+            if 'image.do' in src:
+                var['postThumbnail'].append(
+                    var['channelMainUrl'] + src
+                )
+            else :
+                var['postThumbnail'].append(None)
+        else :
+            var['postThumbnail'].append(None)
+
+        t3wrap = extract_children_tag(li, 'i', {'class' :'t3wrap'}, childIsNotMultiple)
+        spanList = extract_children_tag(t3wrap, 'span', dummyAttrs, childIsMultiple)
+        for spanIdx, span in enumerate(spanList):
+            spanText = extract_text(span)
+            if spanIdx == 0 :
+                var['uploadedTime'].append(
+                    convert_datetime_string_to_isoformat_datetime(
+                        spanText
+                    )
+                )
+            elif spanIdx == 1 :
+                var['uploader'].append(spanText)
+            elif spanIdx == 2 :
+                var['viewCount'].append(
+                    extract_numbers_in_text(spanText)
+                )
+            elif spanIdx == 3 :
+                var['postSubject'].append(
+                    spanText
+                )
+
+    valueList = [var[key] for key in keyList]
+    result = merge_var_to_dict(keyList, valueList)
+    # print(result)
+    return result
+
+def postContentParsingProcess(**params):
+    targetKeyInfo = {
+        'singleType' : ['contact', 'postText'],
+        'multipleType' : ['postImageUrl']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    substance = extract_children_tag(soup, 'div', {'class' : 'substance'}, childIsNotMultiple)
+    postText = extract_text(substance)
+    var['postText'] = clean_text(postText)
+    var['contact'] = extract_contact_numbers_from_text(postText)
+    var['postImageUrl'] = search_img_list_in_contents(substance, var['channelMainUrl'])
+    valueList = [var[key] for key in keyList]
+    result = convert_merged_list_to_dict(keyList, valueList)
+    # print(result)
+    return result
+
+
---
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongnam/cng/scraper.py
@@ -0,0 +1,58 @@
+from workers.data_scraper.scraper_dormitory.scraping_default_usage import Scraper as ABCScraper
+from workers.data_scraper.scraper_dormitory.scraper_tools.tools import *
+from .parser import *
+
+# 채널 이름 : 창녕군청
+
+# 타겟 : 모든 공고
+# 중단 시점 : 마지막 페이지 도달시
+
+#HTTP Request
+'''
+    @post list
+
+    method : GET
+    url =  https://www.cng.go.kr/news/00000372/00000373.web?gcode=1001&cpage={pageCount}
+    header :
+        None
+
+'''
+'''
+    @post info
+    method : GET
+    url : https://www.cng.go.kr/news/00000372/00000373.web?gcode=3376&idx={postId}&amode=view
+    header :
+        None
+
+'''
+sleepSec = 2
+isUpdate = True
+
+class Scraper(ABCScraper):
+    def __init__(self, session):
+        super().__init__(session)
+        self.channelName = '창녕군청'
+        self.postBoardName = '새소식'
+        self.channelMainUrl = 'https://www.cng.go.kr'
+        self.postUrl = 'https://www.cng.go.kr/news/00000372/00000373.web?gcode=3376&idx={}&amode=view'
+        
+    def scraping_process(self, channelCode, channelUrl, dateRange):
+        super().scraping_process(channelCode, channelUrl, dateRange)
+        self.session = set_headers(self.session)
+        self.pageCount = 1
+        while True :
+            self.channelUrl = self.channelUrlFrame.format(self.pageCount)
+            self.post_list_scraping()
+            if self.scrapingTarget :
+                self.target_contents_scraping()
+                self.collect_data()
+                self.mongo.reflect_scraped_data(self.collectedDataList)
+                self.pageCount += 1
+            else :
+                break
+
+    def post_list_scraping(self):
+        super().post_list_scraping(postListParsingProcess, 'get', sleepSec)
+
+    def target_contents_scraping(self):
+        super().target_contents_scraping(postContentParsingProcess, sleepSec)
