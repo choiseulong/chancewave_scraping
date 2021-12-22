3f3ad6b112f2171fdeeb62ccc8fe942925adc4b6
choiseulong <choiseulong@gmail.com>
2021-12-22 18:14:23 +0900

    :sparkles: 채널 hc_0 스크래퍼 추가

---
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongnam/hc/parser.py
@@ -0,0 +1,64 @@
+from workers.data_scraper.scraper_dormitory.parser_tools.tools import *
+
+def postListParsingProcess(**params):
+    targetKeyInfo = {
+        'multipleType' : ['postUrl', 'postTitle', 'uploadedTime', 'viewCount', 'uploader', 'postSubject']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    liList = extract_children_tag(soup, 'li', {'class' : 'li1'}, childIsMultiple)
+    for li in liList:
+        aTag = extract_children_tag(li, 'a', dummyAttrs, childIsNotMultiple)
+        href = extract_attrs(aTag, 'href')
+        var['postUrl'].append(
+            var['postUrlFrame'] + href
+        )
+        strong = extract_children_tag(li, 'strong', {'class' : 't1'}, childIsNotMultiple)
+        var['postTitle'].append(
+            extract_text(strong)
+        )
+        postSubject = extract_children_tag(li, 'em', dummyAttrs, childIsNotMultiple)
+        if postSubject :
+            var['postSubject'].append(
+                extract_text(postSubject)[1:-1]
+            )
+        else :
+            var['postSubject'].append(None)
+        wrap1t3 = extract_children_tag(li, 'i', {'class' :'wrap1t3'}, childIsNotMultiple)
+        spanList = extract_children_tag(wrap1t3, 'span', dummyAttrs, childIsMultiple)
+        for spanIdx, span in enumerate(spanList):
+            spanText = extract_text(span)
+            if spanIdx == 1 :
+                var['uploadedTime'].append(
+                    convert_datetime_string_to_isoformat_datetime(
+                        spanText
+                    )
+                )
+            elif spanIdx == 2 :
+                var['uploader'].append(spanText)
+            elif spanIdx == 3 :
+                var['viewCount'].append(
+                    extract_numbers_in_text(spanText)
+                )
+
+    valueList = [var[key] for key in keyList]
+    result = merge_var_to_dict(keyList, valueList)
+    # print(result)
+    return result
+
+def postContentParsingProcess(**params):
+    targetKeyInfo = {
+        'singleType' : ['contact', 'postText'],
+        'multipleType' : ['postImageUrl']
+    }
+    var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
+    substance = extract_children_tag(soup, 'div', {'class' : 'substance'}, childIsNotMultiple)
+    postText = extract_text(substance)
+    var['postText'] = clean_text(postText)
+    var['contact'] = extract_contact_numbers_from_text(postText)
+    var['postImageUrl'] = search_img_list_in_contents(substance, var['channelMainUrl'])
+    valueList = [var[key] for key in keyList]
+    result = convert_merged_list_to_dict(keyList, valueList)
+    # print(result)
+    return result
+
+
