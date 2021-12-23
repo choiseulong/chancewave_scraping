9e35832977fa35c2f04e1af393aeaa34f688c366
choiseulong <choiseulong@gmail.com>
2021-12-23 16:34:47 +0900

    :sparkles: 채널 sj_0 스크래퍼 추가

--- scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongbuk/sj/parser.py
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongbuk/sj/parser.py
@@ -20,16 +20,16 @@ def postListParsingProcess(**params):
             if tdIdx == 2 :
                 aTag = extract_children_tag(td, 'a', dummyAttrs, childIsNotMultiple)
                 href = extract_attrs(aTag, 'href')
-                postId = extract_numbers_in_text(href)
+                postId = extract_text_between_prefix_and_suffix('bod_uid=', '&pageNo', href)
                 var['postUrl'].append(
                     var['postUrlFrame'].format(postId)
                 )
                 var['postTitle'].append(
                     tdText
                 )
-            elif tdIdx == [1,3] :
+            elif tdIdx == [1] :
                 uploader += tdText + ' '
-            elif tdIdx == 6:
+            elif tdIdx == 5:
                 var['viewCount'].append(
                     extract_numbers_in_text(tdText)
                 )
@@ -49,13 +49,20 @@ def postContentParsingProcess(**params):
         'multipleType' : ['postImageUrl']
     }
     var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
-    cont = extract_children_tag(soup, 'div', {'class' : 'bb-view-content'}, childIsNotMultiple)
+    view_info = extract_children_tag(soup, 'div', {'class' : 'view_info'}, childIsNotMultiple)
+    liList = extract_children_tag(view_info, 'li', dummyAttrs, childIsMultiple)
+    for li in liList:
+        liText = extract_text(li)
+        if '문의처' in liText:
+            var['contact'] = extract_contact_numbers_from_text(liText)
+            break
+
+    cont = extract_children_tag(soup, 'div', {'class' : 'view_cont'}, childIsNotMultiple)
     var['postText'] = extract_text(cont)
-    var['contact'] = extract_contact_numbers_from_text(extract_text(cont))
     var['postImageUrl'] = search_img_list_in_contents(cont, var['channelMainUrl'])
     valueList = [var[key] for key in keyList]
     result = convert_merged_list_to_dict(keyList, valueList)
-    # print(result)
+    print(result)
     return result
 
 
