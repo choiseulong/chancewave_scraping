6d4a5d35b761f57891bb54f6bf975c4f97d394c0
choiseulong <choiseulong@gmail.com>
2021-12-23 16:41:28 +0900

    :sparkles: 채널 yd_0 스크래퍼 추가

--- scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongbuk/yd/parser.py
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/gyeongbuk/yd/parser.py
@@ -17,23 +17,22 @@ def postListParsingProcess(**params):
             #         pass
             #     else :
             #         continue
-            if tdIdx == 2 :
+            if tdIdx == 1 :
                 aTag = extract_children_tag(td, 'a', dummyAttrs, childIsNotMultiple)
                 href = extract_attrs(aTag, 'href')
-                postId = extract_text_between_prefix_and_suffix('bod_uid=', '&pageNo', href)
                 var['postUrl'].append(
-                    var['postUrlFrame'].format(postId)
+                    var['channelMainUrl'] + href
                 )
                 var['postTitle'].append(
                     tdText
                 )
-            elif tdIdx == [1] :
+            elif tdIdx == [2] :
                 uploader += tdText + ' '
-            elif tdIdx == 5:
+            elif tdIdx == 4:
                 var['viewCount'].append(
                     extract_numbers_in_text(tdText)
                 )
-            elif tdIdx == 4:
+            elif tdIdx == 3:
                 var['uploadedTime'].append(
                     convert_datetime_string_to_isoformat_datetime(tdText)
                 )
@@ -49,20 +48,13 @@ def postContentParsingProcess(**params):
         'multipleType' : ['postImageUrl']
     }
     var, soup, keyList, _ = html_type_default_setting(params, targetKeyInfo)
-    view_info = extract_children_tag(soup, 'div', {'class' : 'view_info'}, childIsNotMultiple)
-    liList = extract_children_tag(view_info, 'li', dummyAttrs, childIsMultiple)
-    for li in liList:
-        liText = extract_text(li)
-        if '문의처' in liText:
-            var['contact'] = extract_contact_numbers_from_text(liText)
-            break
-
-    cont = extract_children_tag(soup, 'div', {'class' : 'view_cont'}, childIsNotMultiple)
+    cont = extract_children_tag(soup, 'div', {'class' : 'content-view'}, childIsNotMultiple)
     var['postText'] = extract_text(cont)
+    var['contact'] = extract_contact_numbers_from_text(extract_text(cont))
     var['postImageUrl'] = search_img_list_in_contents(cont, var['channelMainUrl'])
     valueList = [var[key] for key in keyList]
     result = convert_merged_list_to_dict(keyList, valueList)
-    # print(result)
+    print(result)
     return result
 
 
--- scrapingProject/workers/data_scraper/scraping_manager.py
+++ scrapingProject/workers/data_scraper/scraping_manager.py
@@ -56,7 +56,7 @@ class scraping_manager:
                 # something = checker.is_handling(traceback.format_exc(), e.__class__)
                 # print(something)
             
-            if channelCode != 'sj_0':
+            if channelCode != 'yd_0':
                 continue
             print(channelCode, 'init')
             print(groupName, roomName)
