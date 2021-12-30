ea2d882344771fd7e7b8df17cf6cb19660473f2f
keymuz <kanghyun111@gmail.com>
2021-12-30 10:53:29 +0900

    그룹이름, 채널이름, 번호 조합하는 메서드 추가

--- scrapingProject/workers/data_scraper/scraper_dormitory/scraper_tools/tools.py
+++ scrapingProject/workers/data_scraper/scraper_dormitory/scraper_tools/tools.py
@@ -9,46 +9,66 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def set_headers(session, additionalKeyValue=None, isUpdate=False):
+def set_headers(session, additional_key_value=None, is_update=False):
     headers = {
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
     }
-    if additionalKeyValue and isUpdate:
-        for keyValue in additionalKeyValue:
-            headers.update({keyValue[0]:keyValue[1]})
+    if additional_key_value and is_update:
+        for key_value in additional_key_value:
+            headers.update({key_value[0]: key_value[1]})
     session.headers = headers
     return session
 
-def get_method_response(session, url, sleepSec=2):
+
+def extract_channel_board_num(channel_code):
+    """
+    채널 코드에서 채널의 게시판 번호를 추출
+    :param channel_code: 채널 코드
+    :return: int - 게시판 번호
+
+    example)
+    input string "jeonnam__jeonnamdo_0"
+    output int   0
+    """
+    if channel_code.find('_') < 0:
+        return None
+    else:
+        channel_board_num = channel_code.split('_')[-1]
+        return int(channel_board_num)
+
+
+def get_method_response(session, url, sleep_sec=2):
     response = session.get(url, verify=False)
     status = 'fail'
-    if response.status_code == 200 :
+    if response.status_code == 200:
         status = 'ok'
-    sleep(sleepSec)
+    sleep(sleep_sec)
     return status, response
 
-def post_method_response(session, url, data={}, sleepSec=2, jsonize=False):
+def post_method_response(session, url, data={}, sleep_sec=2, jsonize=False):
     if jsonize :
         data = json.dumps(data)
     response = session.post(url, data=data, verify=False)
     status = 'fail'
-    if response.status_code == 200 :
+    if response.status_code == 200:
         status = 'ok'
-    sleep(sleepSec)
+    sleep(sleep_sec)
     return status, response
 
-def filtering_channel_path_in_globals(Globals):
-    channelCodeList = []
-    for globalVarKey in Globals.keys():
-        if 'url_' in globalVarKey:
-            key = globalVarKey.replace("url_", "")
-            channelCodeList.append({key : Globals[f'{globalVarKey}']})
-    return channelCodeList
+def filtering_channel_path_in_globals(globals):
+    channel_code_list = []
+    for global_var_key in globals.keys():
+        if 'url_' in global_var_key:
+            key = global_var_key.replace("url_", "")
+            channel_code_list.append({key : globals[f'{global_var_key}']})
+    return channel_code_list
+
 
 def return_key_value(data):
     key = list(data.keys())[0]
     value = data[key]
     return key, value
+
 
 def get_post_data_frame(
         channelCode='', 
@@ -89,17 +109,19 @@ def get_post_data_frame(
         'extraInfo' : []
     }
 
-def find_key_root(keyName) : 
+
+def find_key_root(key_name) :
     frame = get_post_data_frame()
-    value = frame.get(keyName)
+    value = frame.get(key_name)
     if value is not None:
         return 
     else :
         for key in frame.keys():
-            if type(frame[key]) == dict :
-                value = frame[key].get(keyName)
-                if value is not None :
+            if type(frame[key]) == dict:
+                value = frame[key].get(key_name)
+                if value is not None:
                     return key
+
 
 def enter_data_into_dataFrame(dataFrame, result):
     for key in result:
@@ -107,10 +129,11 @@ def enter_data_into_dataFrame(dataFrame, result):
             dataFrame[key] = result[key]
     return dataFrame
 
-def find_request_params(data, paramsKey):
+
+def find_request_params(data, params_key):
     params = []
-    for key in paramsKey:
-        P = [k for k in data if key in k.keys()]
-        if P:
-            params.append((key,P[0][key]))
+    for key in params_key:
+        tmp_param = [k for k in data if key in k.keys()]
+        if tmp_param:
+            params.append((key, tmp_param[0][key]))
     return params
