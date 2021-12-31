7395a648b7b49113f3af0de673aa8e266bcb740e
choiseulong <choiseulong@gmail.com>
2021-12-31 14:01:02 +0900

    :hammer: 찬스웨이브 시스템 제공 관련 celery broker url 설정 변경

--- scrapingProject/workers/scraping_scheduler/scheduler.py
+++ scrapingProject/workers/scraping_scheduler/scheduler.py
@@ -8,9 +8,9 @@ schedule = Celery('scheduler')
 
 schedule.conf.update(
     broker_url = 'amqp://username:password@localhost//',
-    result_backend = 'mongodb://admin:mysterico@k8s.mysterico.com:31489/?authSource=admin',
-    # broker_url = 'amqp://CHANCEWAVE:MYSTERICO@message_broker_container//',
-    # result_backend = 'mongodb://CHANCEWAVE:MYSTERICO@mongodb_container:27017/?authSource=admin',
+    # result_backend = 'mongodb://admin:mysterico@k8s.mysterico.com:31489/?authSource=admin',
+    broker_url = 'amqp://CHANCEWAVE:MYSTERICO@message_broker_container//',
+    result_backend = 'mongodb://CHANCEWAVE:MYSTERICO@mongodb_container:27017/?authSource=admin',
     timezone = 'Asia/Seoul',
 )
 
