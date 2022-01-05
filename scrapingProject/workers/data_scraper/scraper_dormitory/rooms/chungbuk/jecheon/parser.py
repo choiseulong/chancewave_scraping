5044e3e0071e28f34beb27a4fdbec6ad6d4f435e
choiseulong <choiseulong@gmail.com>
2022-01-05 18:56:10 +0900

    충북 지역 공지사항 12개 스크래퍼 추가

---
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/chungbuk/jecheon/parser.py
@@ -0,0 +1,64 @@
+from workers.data_scraper.scraper_dormitory.parser_tools.tools import *
+
+def post_list_parsing_process(**params):
+    target_key_info = {
+        'multiple_type' : ['post_url', 'uploaded_time', 'post_title', 'view_count', 'uploader', 'post_subject']
+    }
+    var, soup, key_list, _ = html_type_default_setting(params, target_key_info)
+    tbody = extract_children_tag(soup, 'tbody')
+    tr_list = extract_children_tag(tbody, 'tr', is_child_multiple=True)
+    if not tr_list :
+        return
+    # 2021-01-05 header [번호, 카테고리, 제목, 첨부파일, 작성자, 작성일, 조회수]
+    for tr in tr_list :
+        td_list = extract_children_tag(tr, 'td', is_child_multiple=True)
+        td_text = ''
+        for td_idx, td in enumerate(td_list):
+            td_text = extract_text(td).strip()
+            if '공지' in td_text and td_idx == 0 :
+                if var['page_count'] != 1 :
+                    break
+            if td_idx == 1 :
+                var['post_subject'].append(td_text)
+            if td_idx == 2:
+                a_tag = extract_children_tag(td, 'a')
+                href = extract_attrs(a_tag, 'href')
+                var['post_url'].append(
+                    var['post_url_frame'] + href
+                )
+                var['post_title'].append(td_text)
+            elif td_idx == 6:
+                var['view_count'].append(
+                    extract_numbers_in_text(td_text)
+                )
+            elif td_idx == 4:
+                var['uploader'].append(td_text)
+            elif td_idx == 5:
+                var['uploaded_time'].append(
+                    convert_datetime_string_to_isoformat_datetime(td_text)
+                )
+    value_list = [var[key] for key in key_list]
+    result = merge_var_to_dict(key_list, value_list, var['channel_code'])
+    return result
+
+def post_content_parsing_process(**params):
+    target_key_info = {
+        'single_type' : ['post_text', 'contact'],
+        'multiple_type' : ['post_image_url']
+    }
+    var, soup, key_list, _ = html_type_default_setting(params, target_key_info)
+    th_list = extract_children_tag(soup, 'th', child_tag_attrs={'scope' : 'row'}, is_child_multiple=True)
+    for th in th_list:
+        th_text = extract_text(th)
+        if '전화번호' in th_text:
+            var['contact'] = extract_text(find_next_tag(th))
+            break
+    tmp_contents = extract_children_tag(soup, 'td', child_tag_attrs={'class' : 'bbs_content'})
+    var['post_text'] = extract_text(tmp_contents)
+    var['post_image_url'] = search_img_list_in_contents(tmp_contents, var['channel_main_url'])
+    if not var['contact']:
+        var['contact'] = extract_contact_numbers_from_text(extract_text(tmp_contents))
+    value_list = [var[key] for key in key_list]
+    result = convert_merged_list_to_dict(key_list, value_list)
+    return result
+
---
+++ scrapingProject/workers/data_scraper/scraper_dormitory/rooms/chungbuk/jp/scraper.py
@@ -0,0 +1,56 @@
+from workers.data_scraper.scraper_dormitory.scraping_default_usage import Scraper as ABCScraper
+from workers.data_scraper.scraper_dormitory.scraper_tools.tools import *
+from .parser import *
+
+# 채널 이름 : 증평군청
+
+#HTTP Request
+'''
+    @post list
+    method : GET
+    url_0 =  https://www.jp.go.kr/kor/cop/bbs/BBSMSTR_000000000134/selectBoardList.do?\
+        pageIndex={page_count}
+    header :
+        None
+
+'''
+'''
+    @post info
+    method : GET
+    url : 
+        self.post_url 선언시 -
+            self.post_url + href
+        미선언시 -
+            self.channel_main_url + href
+    header :
+        None
+
+'''
+sleep_sec = 1
+
+class Scraper(ABCScraper):
+    def __init__(self, session):
+        super().__init__(session)
+        self.channel_name = '증평군청'
+        self.post_board_name = '공지사항'
+
+    def scraping_process(self, channel_code, channel_url):
+        super().scraping_process(channel_code, channel_url)
+        self.session = set_headers(self.session)
+        self.page_count = 1
+        while True :
+            self.channel_url = self.channel_url_frame.format(self.page_count)
+            self.post_list_scraping()
+            if self.scraping_target :
+                self.target_contents_scraping()
+                self.collect_data()
+                self.mongo.reflect_scraped_data(self.collected_data_list)
+                self.page_count += 1
+            else :
+                break
+
+    def post_list_scraping(self):
+        super().post_list_scraping(post_list_parsing_process, 'get', sleep_sec)
+
+    def target_contents_scraping(self):
+        super().target_contents_scraping(post_content_parsing_process, sleep_sec)
