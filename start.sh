77fd242baa6df6528af113d09aa7528c4aed8ee6
choiseulong <kjkj3468@naver.com>
2023-04-27 11:24:02 +0900

    zip 파일 제외

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ DOCKER_USER="chancewave"
 DOCKER_PASS="ai02140214"
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker-compose up
+
+# docker login --username=chancewave --password=ai02140214
